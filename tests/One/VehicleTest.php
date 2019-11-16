82f924900635cdaf8023026764d7835fda67f6f5
Mior Muhammad Zaki <crynobone@gmail.com>
2019-11-16 09:43:04 +0800

    Improve tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/One/VehicleTest.php
+++ tests/One/VehicleTest.php
@@ -58,6 +58,55 @@ class VehicleTest extends TestCase
     }
 
     /** @test */
+    public function it_can_update_vehicle_information_with_custom_user_uid()
+    {
+        $generator = FakerFactory::create('ms_MY');
+
+        $headers = [
+            'Accept' => 'application/vnd.insure.v1+json',
+            'Authorization' => 'Bearer '.static::ACCESS_TOKEN,
+            'X-Insurance-UID' => 'user-5',
+        ];
+
+        $plateNumber = $generator->jpjNumberPlate;
+        $vehicleInformation = [
+            'chasis_number' => null,
+            'engine_number' => null,
+            'year_manufactured' => 2011,
+            'maker' => 'Peugeot',
+            'model' => '308',
+        ];
+        $ownerInformation = [
+            'fullname' => $generator->name,
+            'birthdate' => '1983-04-03',
+            'nric' => $generator->myKadNumber(),
+            'email' => $generator->email,
+            'phone_no' => $generator->mobileNumber,
+            'postcode' => $generator->postcode,
+        ];
+        $insuranceInformation = [
+            'ended_at' => '2019-05-08 16:00:00',
+        ];
+
+        $payload = array_merge([
+            'plate_number' => $plateNumber,
+            'owner' => $ownerInformation,
+            'insurance' => $insuranceInformation,
+        ], $vehicleInformation);
+
+        $faker = Faker::create()
+                        ->sendJson('POST', $headers, $payload)
+                        ->expectEndpointIs('https://api.insure.drivemark.io/vehicles?include=customer')
+                        ->shouldResponseWith(200, '{}');
+
+        $response = $this->makeClientWithAccessToken($faker)
+                        ->uses('Vehicle')
+                        ->save($plateNumber, $ownerInformation, $insuranceInformation, $vehicleInformation, 'user-5');
+
+        $this->assertInstanceOf(Response::class, $response);
+    }
+
+    /** @test */
     public function it_cant_update_vehicle_information_without_access_token()
     {
         $this->expectException('Katsana\Insurance\Exceptions\MissingAccessToken');
